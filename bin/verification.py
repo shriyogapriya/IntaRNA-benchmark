5ca4f220f99043e04662237ab3a824d2f6cd585a
Yogapriya Ayyanarmoorthy <shriyogapriya@gmail.com>
2019-07-24 10:42:52 +0200

    Comments added

--- bin/verification.py
+++ bin/verification.py
@@ -1,3 +1,15 @@
+"""This Script take the path to the verified-targets.tsv file from input.
+Then, take the input folder (with sequence fasta data) from input.
+ For,  each row from verified targets:
+1)it reads the sRNA sequence
+2)find and reads the target sequence
+3)for each encoded mutation (|-separated)
+3a)report mutation is checked (for debugging and logging)
+4)checks each mutated position if the encoded letter matches the respective letter from the sRNA/target sequence (if not,it reports an error message)
+5)check complementarity of nucleotides before and after mutation, i.e. ensure that they can form a base pair.
+"magic constant 200" has been setup for gene position matching a global constant.
+ """
+
 import csv
 from Bio import SeqIO
 import json
@@ -19,9 +31,9 @@ geneFile = []
 sRNAFile = []
 sRNAGene = {}
 
-constToCheck = 200
+constToCheck = 200    #magic constant 200 for gene position matching
 
-""" parses the tsv file and gets the tabular data inside an arraz named <rows> """
+""" parses the tsv file and gets the tabular data inside an array named <rows> """
 def parseFiletsv():
     with open(filename) as tsvfile:
         tsvreader = csv.reader(tsvfile, delimiter="\t")
@@ -34,10 +46,10 @@ def parseFiletsv():
 """ Segregate the data obtained from the tsv file"""
 def getMutationgene():
     totalOccurances = 0
-    for index, element in enumerate(rows):
+    for index, element in enumerate(rows):                 
         if(len(element) > 6):
             if element[6] != '':
-                if( '@' not in element[6]):
+                if( '@' not in element[6]):    #if no comments found
                     sRNAFile.append(element[0])
                     geneFile.append(element[1])
                     organismFolder.append(element[3])
@@ -94,12 +106,12 @@ def processLoop():
     for index, record in enumerate(sRNAGene):
         mutation = sRNAGene[record].get("mutation")
 
-        if ("|" in mutation):
+        if ("|" in mutation):    #for multiple evalution
             print("\n\n Multiple Evaluations using : \t", mutation)
             multiEval = mutation.split("|")
             for index , element in enumerate(multiEval):
-                part_1, part_2, = element.split('&', 1)
-#                if("-" in part_2):
+                part_1, part_2, = element.split('&', 1)    #SRNA & Gene split
+#                if("-" in part_2):   //"multiple evaluation with  "-" is found"
 #                    negGene(part_2, sRNAGene[record].get("gene").get("Sequence") ,sRNAGene[record].get("gene").get("name"))
 #                else:
                 boolsRNA(part_1, sRNAGene[record].get("fasta") ,sRNAGene[record].get("name"))
@@ -110,14 +122,11 @@ def processLoop():
             print("\n\n Mutation with Bar ',' : \t", mutation)
         else:
             part_1, part_2 = mutation.split('&', 1)
-            if("-" in part_2):
-                negGene(part_2, sRNAGene[record].get("gene").get("Sequence") ,sRNAGene[record].get("gene").get("name"))
-            else:
-                boolsRNA(part_1, sRNAGene[record].get("fasta") ,sRNAGene[record].get("name"))
-                boolsGene(part_2, sRNAGene[record].get("gene").get("Sequence") ,sRNAGene[record].get("gene").get("name"))
-                complementBase(part_2, part_1)
+            boolsRNA(part_1, sRNAGene[record].get("fasta") ,sRNAGene[record].get("name"))
+            boolsGene(part_2, sRNAGene[record].get("gene").get("Sequence") ,sRNAGene[record].get("gene").get("name"))
+            complementBase(part_2, part_1)
 
-
+"SRNA Check"
 def boolsRNA(checkMut, sequence, name):
     position = int(re.findall(r'\d+', checkMut)[0])
     base_1 = checkMut[0].upper()
@@ -132,13 +141,14 @@ def boolsRNA(checkMut, sequence, name):
             # complementBase(base_1, base_2)
         else :
             print("\n  To check ",base_1," in position ",position," in from sequence in file ",name," : Evaluation result Got ",fromSeq," : False")
-            sys.exit()
+            
             # complementBase(base_1, base_2)
     else :
         print("Check for Typo as Sequence is shorter than specified index to check in file", name)
         print("Lenght of Sequence : \t",len(sequence))
         print("Encountered position to find : \t", position)
 
+"Gene Check"
 def boolsGene(checkMut, sequence, name):
     position = constToCheck + int(re.findall(r'-?\d+', checkMut)[0])
     if (position < constToCheck):
@@ -155,13 +165,14 @@ def boolsGene(checkMut, sequence, name):
             # complementBase(base_1, base_2)
         else :
             print("\n  To check ",base_1," in position ",position," in from sequence in file ",name," : Evaluation result Got ",fromSeq," : False")
-            sys.exit()
+            
             # complementBase(base_1, base_2)
     else :
         print("Check for Typo as Sequence is shorter than specified index to check in file", name)
         print("Lenght of Sequence : \t",len(sequence))
         print("Encountered position to find : \t", position)
 
+"negative indices in sRNA"
 def negGene(checkMut, sequence, name):
     seqLen = len(sequence)
     position = constToCheck + int(re.findall(r'-*\d+', checkMut)[0]) +1
@@ -193,7 +204,7 @@ def complementBase(before, after):
         print("Gene complement matched", b_check_1, " <-> ", a_check_1)
     else:
         print("Gene complement mismatched", b_check_1, " <-!WRONG!-> ", a_check_1)
-        exit(-1)
+        
 
     if (((b_check_2 == C or b_check_2 == U) and a_check_2 == G) or (b_check_2 == G and (a_check_2 == C or a_check_2 == U))):
         print("Gene complement matched", b_check_2, " <-> ", a_check_2)
@@ -201,7 +212,7 @@ def complementBase(before, after):
         print("Gene complement matched", b_check_2, " <-> ", a_check_2)
     else:
         print("Gene complement mismatched", b_check_2, " <-!WRONG!-> ", a_check_2)
-        exit(-1)
+       
 
 
 """ Run this only when you want to update the data  """
